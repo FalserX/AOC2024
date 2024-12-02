2463d6fc6a193eb0a8f6a829a6f1182d07a000ce
William Malenfant (FalserX) <22843250+FalserX@users.noreply.github.com>
2024-12-02 13:23:25 -0500

    reformate and begin of part 2 of day 2

--- day2/main.cpp
+++ day2/main.cpp
@@ -1,48 +1,102 @@
 #include <iostream>
 #include "FileOperations.h"
-#include <algorithm>
+#include "algorithm"
 
 using namespace std;
 
-bool isSafeReport(vector<int> report)
+bool isSafeReportWithDampener(vector<int> report)
 {
-    vector<int> increaseDecreaseReportValues;
+    vector<int> increaseDecreaseValues;
+    vector<string> increaseDecreaseState;
     bool safe = true;
-    for (size_t i = 0; i < report.size() - 2; i++)
+    for (size_t reportIdx = 0; reportIdx < report.size() - 1; reportIdx++)
     {
-        if (
-            (report.at(i) < report.at(i + 1) && report.at(i + 1) < report.at(i + 2)) ||
-            (report.at(i) > report.at(i + 1) && report.at(i + 1) > report.at(i + 2)))
+        if (report.at(reportIdx) < report.at(reportIdx + 1))
         {
-            continue;
+            increaseDecreaseState.push_back("Increase");
+        }
+        else if (report.at(reportIdx) > report.at(reportIdx + 1))
+        {
+            increaseDecreaseState.push_back("Decrease");
         }
         else
         {
-            safe = false;
-            break;
+            increaseDecreaseState.push_back("Stable");
         }
+        increaseDecreaseValues.push_back(report.at(reportIdx) - report.at(reportIdx + 1));
     }
-    if (safe)
+    if (
+        (count(increaseDecreaseState.begin(), increaseDecreaseState.end(), "Decrease") >= increaseDecreaseState.size() - 1 &&
+         count(increaseDecreaseState.begin(), increaseDecreaseState.end(), "Increase") <= increaseDecreaseState.size() - 1) ||
+        count(increaseDecreaseState.begin(), increaseDecreaseState.end(), "Increase") >= increaseDecreaseState.size() - 1 &&
+            count(increaseDecreaseState.begin(), increaseDecreaseState.end(), "Decrease") <= increaseDecreaseState.size() - 1)
     {
-        for (size_t i = 0; i < report.size() - 1; i++)
+        string dd = "";
+        for (int increaseDecreaseValue : increaseDecreaseValues)
         {
-            increaseDecreaseReportValues.push_back(report.at(i) - report.at(i + 1));
-        }
-        for (int reportValue : increaseDecreaseReportValues)
-        {
-            if (abs(reportValue) < 1 || abs(reportValue) > 3)
+            if (abs(increaseDecreaseValue) < 1 || abs(increaseDecreaseValue) > 3)
             {
                 safe = false;
                 break;
             }
         }
     }
+    else
+    {
+        safe = false;
+    }
+
+    return safe;
+}
+bool isSafeReport(vector<int> report)
+{
+    vector<int> increaseDecreaseValues;
+    vector<string> increaseDecreaseState;
+    bool safe = true;
+    for (size_t reportIdx = 0; reportIdx < report.size() - 1; reportIdx++)
+    {
+        if (report.at(reportIdx) < report.at(reportIdx + 1))
+        {
+            increaseDecreaseState.push_back("Increase");
+        }
+        else if (report.at(reportIdx) > report.at(reportIdx + 1))
+        {
+            increaseDecreaseState.push_back("Decrease");
+        }
+        else
+        {
+            increaseDecreaseState.push_back("Stable");
+        }
+        increaseDecreaseValues.push_back(report.at(reportIdx) - report.at(reportIdx + 1));
+    }
+    if (
+        (count(increaseDecreaseState.begin(), increaseDecreaseState.end(), "Decrease") >= increaseDecreaseState.size() - 1 &&
+         count(increaseDecreaseState.begin(), increaseDecreaseState.end(), "Increase") <= 0 && count(increaseDecreaseState.begin(), increaseDecreaseState.end(), "Stable") <= 0) ||
+        count(increaseDecreaseState.begin(), increaseDecreaseState.end(), "Increase") >= increaseDecreaseState.size() - 1 &&
+            count(increaseDecreaseState.begin(), increaseDecreaseState.end(), "Decrease") <= 0 && count(increaseDecreaseState.begin(), increaseDecreaseState.end(), "Stable") <= 0)
+    {
+        string dd = "";
+        for (int increaseDecreaseValue : increaseDecreaseValues)
+        {
+            if (abs(increaseDecreaseValue) < 1 || abs(increaseDecreaseValue) > 3)
+            {
+                safe = false;
+                break;
+            }
+        }
+    }
+    else
+    {
+        safe = false;
+    }
+
     return safe;
 }
 
 int main(int argc, char const *argv[])
 {
     int safeReports = 0;
+    int safeReportsDampener = 0;
     FileOperations *operations = new FileOperations();
 
     std::tuple<vector<vector<string>>, FileOperations::FILE_OPERATIONS_FLAGS> result =
@@ -67,13 +121,17 @@ int main(int argc, char const *argv[])
         }
         for (vector<int> report : reports)
         {
-
             if (isSafeReport(report))
             {
                 safeReports++;
             }
+            if (isSafeReportWithDampener(report))
+            {
+                safeReportsDampener++;
+            }
         }
     }
     cout << "The number of safe reports are " << safeReports << endl;
+    cout << "The number of safe reports are " << safeReportsDampener << " with problem dampener" << endl;
     return 0;
 }
